a0a3354ceeafe20c021d4f8839fa3483c1ce9cc0
salimbdi <s_bdirina@estin.dz>
2025-10-04 17:09:56 +0200

    second build

--- src/modules/product/product.validation.ts
+++ src/modules/product/product.validation.ts
@@ -1,26 +1,21 @@
 import { z } from "zod";
 
 export const variantValidationSchema = z.object({
-  type: z.string(),
-  value: z.string(),
+  type: z.string().min(1, "Variant type is required"),
+  value: z.string().min(1, "Variant value is required"),
 });
 
 export const inventoryValidationSchema = z.object({
-  quantity: z.number().min(0),
+  quantity: z.number().min(0, "Quantity must be >= 0"),
   inStock: z.boolean(),
 });
 
 const productValidationSchema = z.object({
-  name: z
-    .string({
-      required_error: "Name of Product is required",
-      invalid_type_error: "Name must be a string",
-    })
-    .min(1, "Name cannot be empty"), // extra validation
-  description: z.string(),
+  name: z.string().min(1, "Name of Product is required"),
+  description: z.string().min(1, "Description is required"),
   price: z.number().positive("Price must be positive"),
-  category: z.string(),
-  tags: z.array(z.string()),
+  category: z.string().min(1, "Category is required"),
+  tags: z.array(z.string().min(1, "Tag cannot be empty")).nonempty("At least one tag is required"),
   variants: z.array(variantValidationSchema),
   inventory: inventoryValidationSchema,
 });
